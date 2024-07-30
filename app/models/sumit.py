7daf81a74e2413c04e3ca489b3fc5deccbb1da90
Mobina Shahbazi <shahbazi.mbn@gmail.com>
2024-07-30 11:49:30 +0330

    relashionships added

--- app/models/sumit.py
+++ app/models/sumit.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, text
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, text, ARRAY, Table
 from sqlalchemy import ForeignKeyConstraint, UniqueConstraint, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
@@ -10,26 +10,62 @@ from sqlalchemy.dialects.mysql import CHAR
 
 from app.db import Base
 
-
+association_table = Table(
+    "association_table",
+    Base.metadata,
+    Column("Health_Service_Center_id", ForeignKey("Health_Service_Center.id")),
+    Column("Doctor_id", ForeignKey("Doctor.id")),
+)
 class Doctor(Base):
     __tablename__ = 'Doctor'
+    __table_args__ = (UniqueConstraint('code', name='_unique_D_code'),)
 
-    id = Column(Integer, primary_key=True)
+    id: Mapped[str] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(255))
+    lastname: Mapped[str] = mapped_column(String(255))
+    code: Mapped[str] = mapped_column(String(255))
+    description: Mapped[Optional[str]] = mapped_column(String(255))
+    address: Mapped[str] = mapped_column(String(255))           #should be removed
+    phone: Mapped[str] = mapped_column(String(255))             #should be removed
+    isDeleted: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    # UUID(as_uuid=False), primary_key=True, index=True, default=uuid.uuid4
-    name = Column(String(255))
-    lastname = Column(String(255))
-    specialization = Column(String(255))
-    code = Column(String(255))
-    description = Column(String(255), nullable=True)
-    address = Column(String(255))
-    phone = Column(String(255))
+    specialty_id: Mapped[int] = mapped_column(ForeignKey("Specialty.id"))
+    specialty: Mapped["Specialty"] = relationship(back_populates="doctors")
 
-    __table_args__ = (
-        UniqueConstraint('code', name='_unique_ws_code'),
-    )
+class Specialty(Base):
+    __tablename__ = 'Specialty'
 
-    # ws_data_records: Mapped[List['WSData']] = relationship()
+    id: Mapped[str] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255))
+    code: Mapped[str] = mapped_column(String(255))
+    description: Mapped[Optional[str]] = mapped_column(String(255))
+    isDeleted: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    doctors: Mapped[List["Doctor"]] = relationship(back_populates="specialty")
+
+class Health_Service_Center(Base):
+    __tablename__ = 'Health_Service_Center'
+
+    id: Mapped[str] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(255))  #enum
+
+    province: Mapped[str] = mapped_column(String(255))
+    district: Mapped[str] = mapped_column(String(255))
+    city: Mapped[str] = mapped_column(String(255))
+    address: Mapped[str] = mapped_column(String(255))
+
+    phone: Mapped[str] = mapped_column(String(255))
+    specialties: Mapped[List[str]] = mapped_column(ARRAY(String(255)))
+    services: Mapped[List[str]] = mapped_column(ARRAY(String(255)))
+    isDeleted: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    doctors: Mapped[List["Doctor"]] = relationship(secondary=association_table)
+
+
+
+
+
+
 
 
 
