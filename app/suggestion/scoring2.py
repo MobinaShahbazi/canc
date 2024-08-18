63cb46113b14810982e091221969d5cc0e53088d
Mobina Shahbazi <shahbazi.mbn@gmail.com>
2024-08-18 17:14:52 +0330

    medical center scoring fixed

--- app/suggestion/scoring2.py
+++ app/suggestion/scoring2.py
@@ -8,14 +8,8 @@ from app.models import Doctor, Medical_Center, Specialty
 def findOptions(db, province, city, specialty, patient_insurer, patient_loc):
     options = []
     distances = []
-    stmt = select(Doctor.id, Medical_Center.province, Doctor.name, Medical_Center.latitude, Medical_Center.longitude,
-                  Medical_Center.province, Doctor.rate, Medical_Center.address, Medical_Center.city).select_from(Doctor) \
-        .join(Specialty, Doctor.specialty_code == Specialty.code) \
-        .join(Medical_Center, Doctor.nezamCode == Medical_Center.doctor_id) \
-        .filter(Specialty.code == specialty)
-        # .join(association_table_D_MC).join(Medical_Center) \
-        # .join(association_table_I_MC).join(Insurer) \
-
+    stmt = select(Medical_Center.province, Medical_Center.city, Medical_Center.address, Medical_Center.latitude, Medical_Center.longitude,
+                  ).select_from(Medical_Center)
 
     results = db.execute(stmt)
     for row in results:
@@ -28,16 +22,16 @@ def findOptions(db, province, city, specialty, patient_insurer, patient_loc):
 
     results = db.execute(stmt)
     for row in results:  # additional info will be added to options
-        options.append([row[0], row[1], findScore(row[5], row[8], row[2], distance(row[3], row[4], patient_loc), province, city, patient_insurer, row[6], max_val, min_val), row[8]])
+        options.append([row[0], row[1], findScore(row[0], row[1], distance(row[3], row[4], patient_loc), province, city, max_val, min_val), row[2]])
 
     sorted_options = sorted(options, key=lambda x: x[2], reverse=True)
     seen_first_parts = {}
     output_list = []
     for item in sorted_options:  # remove repetitive doctors
         first_part = item[0]
-        if first_part not in seen_first_parts:
-            seen_first_parts[first_part] = True
-            output_list.append(item)
+        # if first_part not in seen_first_parts:
+        seen_first_parts[first_part] = True
+        output_list.append(item)
 
     for item in output_list:
         print(item)
@@ -48,13 +42,13 @@ def findOptions(db, province, city, specialty, patient_insurer, patient_loc):
 def distance(lat, lon, patient_loc) -> float:
 
     latitude = float(lat)
-    longetude = float(lon)
-    loc1 = (latitude, longetude)
+    longitude = float(lon)
+    loc1 = (latitude, longitude)
     result = hs.haversine(loc1, patient_loc, unit=Unit.KILOMETERS)
     return result
 
 
-def findScore(province, city, insurer, distance, patient_prov, patient_city, patient_insurer, dr_rate, max_val, min_val):
+def findScore(province, city, distance, patient_prov, patient_city, max_val, min_val):
 
     if province == patient_prov:
         x1 = 1
@@ -68,10 +62,6 @@ def findScore(province, city, insurer, distance, patient_prov, patient_city, pat
 
     x3 = (distance - min_val) / (max_val - min_val)
 
-    if dr_rate is not np.nan:
-        x4 = (dr_rate) / (5)
-    else:
-        x4 = 0.5
-    score = 10*x1 + 1*x2 - 1*x3 + 1*x4
+    score = 10*x1 + 1*x2 - 1*x3
     return score
 
